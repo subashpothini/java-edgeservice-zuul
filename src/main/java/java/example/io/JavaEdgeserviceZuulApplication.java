87407a7948dc5c08c480adb6e135ab6039880d40
Alfa Irawan <alfa.irawan@outlook.com>
2016-12-21 17:31:20 +0700

    First initial 20161221.1731

---
+++ src/main/java/java/example/io/JavaEdgeserviceZuulApplication.java
@@ -0,0 +1,12 @@
+package java.example.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEdgeserviceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEdgeserviceZuulApplication.class, args);
+	}
+}
